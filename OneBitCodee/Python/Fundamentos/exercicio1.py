5d6bf378d7d2a4f97ac6edf0dfe9ee22a1e78eab
Gabriel Azevedo Alvarenga <162355093+gazved@users.noreply.github.com>
2024-12-25 19:58:05 -0300

    Add files via upload

---
+++ OneBitCodee/Python/Fundamentos/exercicio1.py
@@ -0,0 +1,16 @@
+num = int(input("Digite um numero: "))
+num += 1
+suc = num
+num -=2
+ant = num 
+
+print(f"Antecessor: {ant}\n Sucessor: {suc}")
+
+
+print("Digite 4 numeros:")
+num1 = int(input("primeiro: "))
+num2 = int(input("segundo: "))
+num3 = int(input("terceiro: "))
+num4 = int(input("quarto: "))
+media = (num1 + num2 + num3 + num4)/4
+print(f"media = {media:.2f}")
