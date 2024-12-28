b556fe723a2f342a3cbd9225d30046845e641e3e
Gabriel Azevedo Alvarenga <gazevedoalvarenga@gmail.com>
2024-12-27 23:31:10 -0300

    .

--- OneBitCodee/Python/Fundamentos/12-tuplas.py
+++ OneBitCodee/Python/Fundamentos/12-tuplas.py
@@ -2,3 +2,9 @@ gamesTuple = ("Fifa23", "Red Dead 2", "Star Wars", "The Legend of Zelda", "Red D
 
 #completamente imutavel
 
+print(gamesTuple[:2])
+print(gamesTuple[-1])
+
+print(gamesTuple.index("Fifa23"))
+
+print(gamesTuple.count("Red Dead 2"))
