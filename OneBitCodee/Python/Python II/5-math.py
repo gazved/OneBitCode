45608745d602799d3988cd7b1a4d00fa69f5b01e
Gabriel Azevedo Alvarenga <gazevedoalvarenga@gmail.com>
2025-01-17 00:41:02 -0300

    .

---
+++ OneBitCodee/Python/Python II/5-math.py
@@ -0,0 +1,11 @@
+import math
+
+
+print(math.pi)
+print(f"{math.pi:.2f}")
+
+
+num1 = 10.4
+print(math.ceil(num1))
+print(math.floor(num1))
+print(math.factorial(num1))
--- OneBitCodee/Python/Python II/exercicio2.py
+++ OneBitCodee/Python/Python II/exercicio2.py
@@ -1,2 +1,17 @@
 import os
 help('os')
+
+os.system('shutdown /s')#desliga em 60 seg
+os.system('shutdown /s /t 0')#desliga imediatamente
+
+os.system('shutdown /a')#cancela o desligamento
+
+
+def turn_off_one_hour():
+    os.system('shutdown /s /t 3600')
+
+def turn_off_half_an_hour():
+    os.system('shutdown /s /t 1800')
+
+def cancel_shutdown():
+    os.system('shutdown /a')
